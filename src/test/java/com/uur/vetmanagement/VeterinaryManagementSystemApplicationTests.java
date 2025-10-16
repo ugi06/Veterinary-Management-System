ba1e0960910cc669bcf7eddbf817065c8f4992b4
Uğur Taşdelen <ugurtasdelen1234@gmail.com>
2025-10-16 11:40:07 +0300

    start

---
+++ src/test/java/com/uur/vetmanagement/VeterinaryManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uur.vetmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VeterinaryManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
