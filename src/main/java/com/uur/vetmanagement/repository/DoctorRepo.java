800e63f811cb19c31d718cf689b6e2bcbcc3908d
Uğur Taşdelen <ugurtasdelen1234@gmail.com>
2025-10-26 22:45:19 +0300

    DoctorRepo updated

--- src/main/java/com/uur/vetmanagement/repository/DoctorRepo.java
+++ src/main/java/com/uur/vetmanagement/repository/DoctorRepo.java
@@ -4,7 +4,6 @@ import com.uur.vetmanagement.entity.Doctor;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-
 @Repository
 public interface DoctorRepo extends JpaRepository<Doctor,Long> {
 }
