600beea0018d8e89f98795b653a71320b2cfed53
Uğur Taşdelen <ugurtasdelen1234@gmail.com>
2025-10-26 22:44:54 +0300

    Result, ResultData updated

---
+++ src/main/java/com/uur/vetmanagement/core/result/Result.java
@@ -0,0 +1,20 @@
+package com.uur.vetmanagement.core.result;
+
+import lombok.Getter;
+
+@Getter
+public class Result {
+
+    private boolean status;
+
+    private String message;
+
+    private String httpCode;
+
+    public Result(boolean status, String message, String httpCode) {
+        this.status = status;
+        this.message = message;
+        this.httpCode = httpCode;
+    }
+
+}
---
+++ src/main/java/com/uur/vetmanagement/core/result/ResultData.java
@@ -0,0 +1,37 @@
+package com.uur.vetmanagement.core.result;
+
+import com.uur.vetmanagement.core.config.exception.Msg;
+import lombok.Getter;
+
+@Getter
+public class ResultData <T> extends Result{
+
+    private T data;
+
+
+    public ResultData(boolean status, String message, String httpCode,T data) {
+        super(status, message, httpCode);
+        this.data=data;
+    }
+
+    public static <T>ResultData<T> created(T data){
+        return new ResultData<>(true, Msg.CREATED,"201",data);
+    }
+
+    public static <T>ResultData<T> validationError(T data){
+        return new ResultData<>(false, Msg.INVALID,"400",data);
+    }
+
+
+    public static <T>ResultData<T> success(T data){
+        return new ResultData<>(true, Msg.OK,"200",data);
+    }
+
+    public static Result notFoundException(String msg){
+        return new Result(false,msg,"404");
+    }
+
+
+
+
+}
